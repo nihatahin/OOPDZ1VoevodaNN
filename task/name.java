468397cf85adcb26fa57bb2c10b4dc88bd3f8f2f
nihatahin <threespots@mail.ru>
2022-12-17 20:00:04 +0300

    Class name was created. Constructor and get function were added.

---
+++ task/name.java
@@ -0,0 +1,16 @@
+package task;
+public class name {
+    private String nm;
+    name(String val)
+    {
+        nm = val;
+    }
+    
+    public String getName()
+    {
+        return nm;
+    }
+
+    
+    
+}
