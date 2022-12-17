981c0e55e34cf60c2967b01d65cddba2984c092d
nihatahin <threespots@mail.ru>
2022-12-17 20:33:34 +0300

    Find by name part logic was added.

---
+++ task/task.java
@@ -0,0 +1,23 @@
+package task;
+
+
+public class task {
+
+    public static void main(String[] args) {
+        name groad = new name("fabuLa");
+
+        System.out.println(groad.isStrInName("vae"));
+        System.out.println(groad.isStrInName("f"));
+        System.out.println(groad.isStrInName("a"));
+        System.out.println(groad.isStrInName("b"));
+        System.out.println(groad.isStrInName("u"));
+        System.out.println(groad.isStrInName("l"));
+        System.out.println(groad.isStrInName("a"));
+        System.out.println(groad.isStrInName("fa"));
+        System.out.println(groad.isStrInName("bU"));
+        System.out.println(groad.isStrInName("la"));
+        System.out.println(groad.isStrInName("fbb"));
+    }
+    
+    
+}
